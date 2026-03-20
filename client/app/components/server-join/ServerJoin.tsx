8958aab016cd3a12aa002a28b544432e4b605002
Danny Dang <dannydang2423@gmail.com>
2026-03-20 14:55:29 -0400

    Feature: update updated UI

--- client/app/components/server-join/ServerJoin.tsx
+++ client/app/components/server-join/ServerJoin.tsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "~/components/ui/field"
+
 
 interface ServerJoinProps {
   submitForm: (serverIP: string, username: string, password: string) => void;
@@ -13,28 +28,48 @@ export default function ServerJoin({ submitForm }: ServerJoinProps) {
 
   return (
     <div className="max-w-sm m-auto flex flex-col h-screen justify-center">
-      <h1>Server info</h1>
-      <Input
-        placeholder="Server IP"
-        value={serverIP}
-        onChange={(e) => setServerIP(e.target.value)}
-      />
-      <Input
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <Input
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button
-        disabled={!serverIP || !username || !password}
-        onClick={() => submitForm(serverIP, username, password)}
-      >
-        Login
-      </Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to The Server</CardTitle>
+          <CardDescription>Enter Server Details to Login</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <Input
+                  placeholder="Server IP"
+                  value={serverIP}
+                  onChange={(e) => setServerIP(e.target.value)}
+                  required
+                />
+              </Field>
+              <Field>
+                <Input
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </Field>
+              <Field>
+                <Input
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
